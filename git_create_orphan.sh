9bcd8237fcf61a1dc06bbe3b8d8f0bef30b3e296
hatifnaqvi <hatif4@gmail.com>
2016-12-27 13:32:23 -0700

    Delete git_create_orphan.sh

--- git_create_orphan.sh
+++
@@ -1,8 +0,0 @@
-cd repository
-git checkout --orphan orphan_name
-git rm -rf .
-rm '.gitignore'
-echo "#Title of Readme" > README.md
-git add README.md
-git commit -a -m "Initial Commit"
-git push origin orphan_name
